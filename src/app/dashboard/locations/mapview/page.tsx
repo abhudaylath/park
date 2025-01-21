70e64198a7eb89c97a5e28a10cb5c1eb1e06cb76
Abhuday Lath <abhudaylath@gmail.com>
2025-01-21 11:46:50 +0530

    Sixth commit

--- src/app/dashboard/locations/mapview/page.tsx
+++ src/app/dashboard/locations/mapview/page.tsx
@@ -1,9 +1,29 @@
+import Map from '@/components/map'
+import { connectToDB } from '@/lib/db'
+import { ParkingLocation, ParkingLocationModel } from '@/schemas/parking-locations'
+import { MapAddressType, MapParams, ParkingLocationStatus } from '@/types'
 import React from 'react'
 
 async function LocationMapViewPage() {
+
+    await connectToDB()
+
+    const parkingLocations: ParkingLocation[] = await ParkingLocationModel.find({})
+
+    const params: MapParams[] = parkingLocations.filter(loc => loc.status === ParkingLocationStatus.AVAILABLE)
+        .map(loc => ({
+            address: loc.address,
+            gpscoords: loc.gpscoords,
+            price: loc.price,
+            numberofspots: loc.numberofspots,
+            status: loc.status,
+            type: MapAddressType.ADMIN,
+            id: loc.id
+        }))
+
     return (
         <div className='p-2'>
-            Mapview
+            <Map mapParams={JSON.stringify(params)} />
         </div>
     )
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -35,3 +35,86 @@ if (index !== -1) {
 }
 return b;
 }
+
+
+
+export const buildMapInfoCardContent = (title: string, address: string, totalSpots: number, price: number)
+: string => {
+
+  return `
+    <div class="map_infocard_content">
+      <div class="map_infocard_title">${title}</div>
+      <div class="map_infocard_body">
+      <div>${address}</div>
+      <hr />
+      <div>Total spots: ${totalSpots}</div>
+      <div>Hourly price: ${formatAmountForDisplay(price, 'CAD')}</div>
+      </div>
+      
+  </div>
+  `
+}
+
+
+export const buildMapInfoCardContentForDestination = (title: string, address: string): string => {
+  return `
+  <div class="map_infocard_content">
+      <div class="map_infocard_title">${title}</div>
+      <div class="map_infocard_body">
+      <div>${address}</div>
+      </div>
+      
+  </div>`;
+}
+
+
+
+export const destinationPin = (type: string) => {
+  const glyphImg = document.createElement('img');
+  glyphImg.src = `http://localhost:3000/${type}.png`;
+  const pinElement = new google.maps.marker.PinElement({
+      glyph: glyphImg
+  })
+
+  return pinElement
+}
+
+
+
+export const parkingPin = (type: string) => {
+  const glyphImg = document.createElement('div')
+  glyphImg.innerHTML = `
+    <div class="map_pin_container">
+      <img src='http://localhost:3000/${type}.png' />
+    </div>
+  `
+
+  const pinElement = new google.maps.marker.PinElement({
+    glyph: glyphImg
+  })
+
+  return pinElement
+}
+
+
+
+export const parkingPinWithIndex = (type: string, index: number) => {
+  const glyphImg = document.createElement('div')
+  glyphImg.innerHTML = `
+    <div class="map_pin_container">
+      <div class="map_pin_id"><span>${index}</span></div>
+      <img src='http://localhost:3000/${type}.png' />
+    </div>
+  `
+
+  const pinElement = new google.maps.marker.PinElement({
+    glyph: glyphImg
+  })
+
+  return pinElement
+}
+
+
+
+
+
